8375f333078d1c11e976f27c1f28e5444b7b5ff6
pundirgit <87014814+pundirgit@users.noreply.github.com>
2022-06-08 17:37:06 +0530

    getAllEmployees using mongoose

--- Database/mongooesdb.js
+++ Database/mongooesdb.js
@@ -1,18 +1,27 @@
 import mongoose from "mongoose";
 import EmployeeModel from "../Model/Employee/EmployeeModel";
 
-mongoose.connect("mongodb+srv://rpundir1:r1QZ79y04BkApMir@cluster0.lqc45.mongodb.net/mydb?retryWrites=true&w=majority").then(()=>{
+mongoose
+  .connect(
+    "mongodb+srv://rpundir1:r1QZ79y04BkApMir@cluster0.lqc45.mongodb.net/mydb?retryWrites=true&w=majority"
+  )
+  .then(() => {
     console.log("database connection successfully");
-}).catch(()=>{
+  })
+  .catch(() => {
     console.log("database connection failed");
-})
+  });
 
 export const createNewEmployee = async (req, res, next) => {
-    const createdEmployee = new EmployeeModel();
-    createdEmployee.empName = req.body.empName;
-    createdEmployee.empEmail = req.body.empEmail;
+  const createdEmployee = new EmployeeModel();
+  createdEmployee.empName = req.body.empName;
+  createdEmployee.empEmail = req.body.empEmail;
 
-    const result = await createdEmployee.save();
-    return res.json(result);
+  const result = await createdEmployee.save();
+  return res.json(result);
 };
 
+export const getAllEmployee = async (req, res, next) => {
+    const result = await EmployeeModel.find().exec();
+    return res.json(result);
+};
--- app2.js
+++ app2.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createNewEmployee } from "./Database/mongooesdb";
+import { createNewEmployee, getAllEmployee } from "./Database/mongooesdb";
 
 const app = express();
 
 app.use(express.json())
 
+app.get("/getEmployee", getAllEmployee)
 app.post("/createNewEmployee", createNewEmployee)
+
 
 app.listen(5000);
